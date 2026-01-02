f80a8589241d9fce68d51bc0d859a1b95cb8d5cb
RobertoA1 <duckyroberto@gmail.com>
2026-01-01 23:59:43 -0500

    refactor(backend): Añadido métodos helpers para conocer y obtener Conceptos de Accion (clase ConceptosAccion)

--- app/Models/ConceptoAccion.php
+++ app/Models/ConceptoAccion.php
@@ -11,21 +11,87 @@ class ConceptoAccion extends Model
 
     protected $table = 'concepto_accion';
 
-    protected $primaryKey = 'id_concepto_accion'; 
+    protected $primaryKey = 'id_concepto_accion';
     public $incrementing = true;
-    protected $keyType = 'int'; 
+    protected $keyType = 'int';
+
+    public const ID_VER = 1;
+    public const ID_EDITAR = 2;
+    public const ID_ELIMINAR = 3;
+    public const ID_RESTAURAR = 4;
+    public const ID_CREAR = 5;
 
     protected $fillable = [
         'accion',
         'estado',
     ];
- 
+
     protected function casts(): array
     {
         return [
-            'estado' => 'boolean', 
+            'estado' => 'boolean',
         ];
     }
 
+    public static function getAccionById(int $id): ConceptoAccion
+    {
+        return match ($id) {
+            self::ID_VER => self::accionVer(),
+            self::ID_EDITAR => self::accionEditar(),
+            self::ID_ELIMINAR => self::accionEliminar(),
+            self::ID_RESTAURAR => self::accionRestaurar(),
+            self::ID_CREAR => self::accionCrear(),
+            default => throw new \Exception('Acción no encontrada'),
+        };
+    }
 
+    public static function accionVer(): ConceptoAccion
+    {
+        return self::find(self::ID_VER);
+    }
+
+    public static function accionEditar(): ConceptoAccion
+    {
+        return self::find(self::ID_EDITAR);
+    }
+
+    public static function accionEliminar(): ConceptoAccion
+    {
+        return self::find(self::ID_ELIMINAR);
+    }
+
+    public static function accionRestaurar(): ConceptoAccion
+    {
+        return self::find(self::ID_RESTAURAR);
+    }
+
+    public static function accionCrear(): ConceptoAccion
+    {
+        return self::find(self::ID_CREAR);
+    }
+
+    public function esAccionVer(): bool
+    {
+        return $this->getKey() === self::ID_VER;
+    }
+
+    public function esAccionEditar(): bool
+    {
+        return $this->getKey() === self::ID_EDITAR;
+    }
+
+    public function esAccionEliminar(): bool
+    {
+        return $this->getKey() === self::ID_ELIMINAR;
+    }
+
+    public function esAccionRestaurar(): bool
+    {
+        return $this->getKey() === self::ID_RESTAURAR;
+    }
+
+    public function esAccionCrear(): bool
+    {
+        return $this->getKey() === self::ID_CREAR;
+    }
 }
